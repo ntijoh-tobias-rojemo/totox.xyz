3623e7f71bc2cd16ee82c0562730af18d7d01e16
tobias-rojemo-nti-johanneberg <tobias.rojemo@elev.ga.ntig.se>
2022-03-14 12:56:12 +0100

    bg replacer is now asynchronous

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
   
 });
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   const background = document.getElementById('desktop-bg');
   background.src = 'img/desktop-bg.jpg';
 });
