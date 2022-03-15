3723caa1f96a7bc5d2a409f2c92890e51a879acc
tobias-rojemo-nti-johanneberg <tobias.rojemo@elev.ga.ntig.se>
2022-03-15 09:53:32 +0100

    Improved loading for background image on mobile Adjusted font sizes Added lorem ipsum to empty rpg.html articles

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
 const isDesktop = window.matchMedia("only screen and (min-aspect-ratio:6/5)");
 
+window.addEventListener('DOMContentLoaded', async () => {
+  if (isDesktop.matches) {
+    const background = document.getElementById('desktop-bg');
+    background.src = 'img/desktop-bg-lq.jpg';
+  }
+});
+
 window.addEventListener('load', updateBG);
 window.addEventListener('resize', updateBG);
 
@@ -7,8 +14,5 @@ async function updateBG() {
   const background = document.getElementById('desktop-bg');
   if (isDesktop.matches) {
     background.src = 'img/desktop-bg.jpg';  
-  }
-  else {
-    background.src = 'data:';  
   }
 }
