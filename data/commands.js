88f7a0d2d3c0155a6b094d8913ff7eaebfbd146a
tobias-rojemo-nti-johanneberg <tobias.rojemo@elev.ga.ntig.se>
2022-03-28 13:24:12 +0200

    lorem ipsum

---
+++ data/commands.js
@@ -0,0 +1,53 @@
+const commands = [
+  {
+    "name": "sudo",
+    "perms": "admin",
+    "usage": "<...>",
+    "short": "Various commands for management of the bot",
+    "desc": "Don't worry about it"
+  },
+  {
+    "name": "1",
+    "perms": "1",
+    "usage": "1",
+    "short": "1",
+    "desc": "1"
+  },
+  {
+    "name": "2",
+    "perms": "2",
+    "usage": "2",
+    "short": "2",
+    "desc": "2"
+  },
+  {
+    "name": "3",
+    "perms": "3",
+    "usage": "3",
+    "short": "3",
+    "desc": "3"
+  },
+  {
+    "name": "4",
+    "perms": "4",
+    "usage": "4",
+    "short": "4",
+    "desc": "4"
+  },
+  {
+    "name": "5",
+    "perms": "5",
+    "usage": "5",
+    "short": "5",
+    "desc": "5"
+  },
+  {
+    "name": "6",
+    "perms": "6",
+    "usage": "6",
+    "short": "6",
+    "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, numquam deserunt. Iste, architecto asperiores consequuntur neque ad, quis corporis hic soluta odit saepe temporibus maiores inventore illo reiciendis voluptas explicabo molestiae, maxime ipsam id. A quo neque labore et obcaecati perspiciatis magni id molestiae, asperiores voluptas, expedita nostrum nulla, possimus nihil?<br> Impedit nemo incidunt doloribus error molestias voluptates voluptate eveniet vel ratione laudantium!<br> Natus excepturi mollitia possimus iusto inventore voluptatum, quod ea animi similique nam deleniti odio officia corporis dolores unde. Facilis ea consequatur labore, autem necessitatibus quia nemo enim ex, fuga reiciendis molestias omnis sapiente deleniti aut consequuntur harum."
+  }
+];
+
+export {commands as commands};
